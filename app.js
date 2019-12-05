d8ef2377a19675a45fd8264a9a071880422b3e42
Stephanie A <stephanie.m.aurelio@gmail.com>
2019-12-05 13:11:29 -0800

    created file structures, html templates, and role classes

---
+++ app.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+const inquirer = require("inquirer");
+const util = require("util");
+
+// Function to collect user input for Team Members
+// Need to collect parameters for Employee class then:
+// based on response to role, ask follow up questions for each team member
+// Only create HTML file if team is complete
+
+// Function to write the collected data into HTML file
+
+// Init function to launch application when run in node
